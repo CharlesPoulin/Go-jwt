2f47e0ac9fb25d680b5bbd8baa7293e8191acf53
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2024-04-03 23:08:16 -0400

    Connection start to Db MYSQL
    no Db created

---
+++ initializers/connectToDb.go
@@ -0,0 +1,12 @@
+package initializers
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectToDb() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+}
